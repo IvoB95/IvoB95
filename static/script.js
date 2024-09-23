797fb11b482e0f9bee111e4da8c7562b11c10853
IvoB95 <ivoberts@gmail.com>
2024-09-23 21:56:02 +0200

    improve style and buttons

--- static/script.js
+++ static/script.js
@@ -20,3 +20,52 @@ function onSectionButtonClick() {
         openCloseMenu()
     }
 }
+
+function activateCorrectSideBarButton() {
+    const itemPanels = document.getElementsByClassName("content-outer");
+    const closestPanel = getClosestItemPanel(itemPanels);
+    setButtonActivations(closestPanel.id);
+}
+
+function getClosestItemPanel(itemPanels) {
+    let closestPanel;
+    let lowest = Number.POSITIVE_INFINITY;
+
+    for (var itemPanel of itemPanels) {
+        const panelDistance = getItemPanelDistance(itemPanel);
+
+        if (panelDistance < lowest) {
+            closestPanel = itemPanel;
+            lowest = panelDistance;
+        }
+    }
+
+    return closestPanel;
+}
+
+function getItemPanelDistance(itemPanel) {
+    const boundingRect = itemPanel.getBoundingClientRect();
+    return Math.abs(boundingRect.y);
+}
+
+function setButtonActivations(itemId) {
+    var sideBarButtons = getAllSideBarButtons();
+
+    for (var sideBarButton of sideBarButtons) {
+        if (isMatchingSideBarButton(sideBarButton, itemId)) {
+            sideBarButton.classList.add('active');
+        } else {
+            sideBarButton.classList.remove('active');
+        }
+    }
+}
+
+function getAllSideBarButtons() {
+    var navigationList = document.getElementById('navigation_list');
+
+    return navigation_list.getElementsByTagName('a');
+}
+
+function isMatchingSideBarButton(sideBarButton, itemId) {
+    return sideBarButton.dataset['panelId'] === itemId;
+}
